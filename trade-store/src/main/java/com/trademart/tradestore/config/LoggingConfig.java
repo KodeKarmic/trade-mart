76af4741ec5e40ac2093aed290fae4b48b4e0e32
Piyush Rathi <jst4upiyush@gmail.com>
2025-10-27 03:05:00 +0530

    chore(trade-store): add Spotless and Checkstyle config; add minimal LoggingConfig

---
+++ trade-store/src/main/java/com/trademart/tradestore/config/LoggingConfig.java
@@ -0,0 +1,20 @@
+package com.trademart.tradestore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(false);
+    loggingFilter.setMaxPayloadLength(1000);
+    loggingFilter.setIncludeHeaders(false);
+    return loggingFilter;
+  }
+}
